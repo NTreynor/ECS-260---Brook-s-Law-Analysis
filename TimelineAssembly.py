e8e315b56a163476f323f78d7d262e866726df82
Nicholas Treynor <Ntreynor@gmail.com>
2021-11-05 15:38:21 -0700

    Added new "Author" objects, and pass over commit data in order to obtain dates of first and final commits for every author in a given repo.

--- TimelineAssembly.py
+++ TimelineAssembly.py
@@ -2,10 +2,21 @@ from pydriller import Repository
 from pydriller.metrics.process.code_churn import CodeChurn
 from datetime import datetime, timedelta
 
+
 # Thought process: For a given github repo, our first task is to assemble a list of authors, and their start and end dates of active contribution.
 # For now, we will define this as the period of time between their first and their final commit in a given repo. Once we have this data, we assemble a timeline
 # by filling an array with the start and end dates of every author in a project (and information allowing for the retrieval of the author's id associated with said
 # start or end date. A second array will consist of start and end dates, and the number of active developers present at the start of a given date.
+
+class Author:
+    def __init__(self, id, first_commit, last_commit):
+        self.name = id
+        self.first_commit = first_commit
+        self.last_commit = last_commit
+
+    def update_last_commit(self, new_last_commit):
+        self.last_commit = new_last_commit
+
 
 def main():
     testUrl = ["https://github.com/Leaflet/Leaflet"]
@@ -13,24 +24,35 @@ def main():
 
     return uniqueAuthors
 
+
 def populateAuthors(repoUrl):
     repo_authors = []
+    repo_author_objects = []
     for commit in Repository(path_to_repo=repoUrl).traverse_commits():
         current_author = commit.author.name
-        if current_author in repo_authors:
+        if current_author in repo_authors: # A more recent commit by this author has been found
+            for x in repo_author_objects:
+                if x.name == current_author: # so we find the appropriate author object
+                    x.last_commit = commit.author_date # And update it's date.
+                    print("New commit by " + current_author)
+                break
             continue
-        else:
+        else: # This is the first instance this author has been detected. Instantiate a new author object.
             repo_authors.append(current_author)
-            print("New Author Found: " + current_author )
+            new_author = Author(current_author, commit.author_date, commit.author_date)
+            repo_author_objects.append(new_author)
+            print("New Author Found: " + current_author)
     return repo_authors
+
+# There should be a method to update this such that we can perform all updates in one pass to improve time complexity, I beleive.
+
 
 # def findFirstCommit(author, repoUrl):
 
-    # TODO: Find date of first commit from author
+# TODO: Find date of first commit from author
 
 # def findLastCommit(author, repoUrl):
 
-    # TODO: Find date of last commit from author
+# TODO: Find date of last commit from author
 
 main()
-
