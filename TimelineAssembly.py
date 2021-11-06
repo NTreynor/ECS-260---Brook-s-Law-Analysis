6e8808ca9c1b981acce444df0971494fe2057d82
Jamie <jnguyen127@horizon.csueastbay.edu>
2021-11-06 16:36:43 -0700

    Commented unncessary print statements. Added function to calculate average commit count

--- TimelineAssembly.py
+++ TimelineAssembly.py
@@ -1,5 +1,6 @@
 from pydriller import Repository
 from pydriller.metrics.process.code_churn import CodeChurn
+from pydriller.metrics.process.commits_count import CommitsCount
 from datetime import datetime, timedelta
 
 
@@ -36,19 +37,21 @@ def main():
     for x in testUrl:
         uniqueAuthors, author_objects = populateAuthors(x)
 
-        print(len(uniqueAuthors))
-        print(len(author_objects))
+        # print(len(uniqueAuthors))
+        # print(len(author_objects))
         #print(str(author_objects[0]))
         #print(str(author_objects[1]))
         #print(str(author_objects[2]))
 
         timeline = populateTimeline(author_objects)
         intervals = locatePairedTwoWeekPlusIntervals(timeline)
-        print(len(intervals))
+        # print(len(intervals))
     
         calculate_code_churn(x, intervals)
+        calculate_commit_count(x, intervals)
 
 def calculate_code_churn(repo, interval_list):
+    # Loops through all significant intervals of development of a given repository and calculators the average code churn per day
     for i in range(0, len(interval_list)): 
         pre_start_date = interval_list[i][0].date
         pre_end_date = interval_list[i][1].date
@@ -60,22 +63,46 @@ def calculate_code_churn(repo, interval_list):
         post_metric = CodeChurn(path_to_repo=repo, since=pre_end_date, to=post_end_date)
         post_churn_total = post_metric.count()
         
-        print("Interval %d" %(i+1))
+        print("Average Code Churn - Interval %d" %(i+1))
         
         values_array = pre_churn_total.values()
         pre_days_difference = abs(pre_start_date - pre_end_date).total_seconds() / 86400.0
         avg_churn = sum(values_array)/pre_days_difference
-        print("Average code churn for pre-period", pre_start_date, pre_end_date, avg_churn)
+        print("Average code churn for pre-period", pre_start_date, "to", pre_end_date + ":", avg_churn)
         
         values_array = post_churn_total.values()
         post_days_difference = abs(post_end_date - pre_end_date).total_seconds() / 86400.0
         avg_churn = sum(values_array)/post_days_difference
-        print("Average code churn for post-period", pre_end_date, post_end_date, avg_churn)
+        print("Average code churn for post-period", pre_end_date, "to", post_end_date + ":", avg_churn)
     
+def calculate_commit_count(repo, interval_list):
+    for i in range(0, len(interval_list)): 
+        pre_start_date = interval_list[i][0].date
+        pre_end_date = interval_list[i][1].date
+        post_end_date = interval_list[i][2].date
+        
+        pre_metric = CommitsCount(path_to_repo=repo, since=pre_start_date, to=pre_end_date)
+        pre_cc_total = pre_metric.count()    # Returns the total code churn in range
+        
+        post_metric = CommitsCount(path_to_repo=repo, since=pre_end_date, to=post_end_date)
+        post_cc_total = post_metric.count()
+        
+        print("Average Commit Count - Interval %d" %(i+1))
+        
+        values_array = pre_cc_total.values()
+        pre_days_difference = abs(pre_start_date - pre_end_date).total_seconds() / 86400.0
+        avg_cc = sum(values_array)/pre_days_difference
+        print("Average commit count for pre-period", pre_start_date, "to", pre_end_date + ":", avg_cc)
+        
+        values_array = post_cc_total.values()
+        post_days_difference = abs(post_end_date - pre_end_date).total_seconds() / 86400.0
+        avg_cc = sum(values_array)/post_days_difference
+        print("Average commit count for post-period", pre_end_date, "to", post_end_date + ":", avg_cc)
+        
     
 
 def locatePairedTwoWeekPlusIntervals(timeline):
-    print("Attempting to locate paired two week intervals")
+    # print("Attempting to locate paired two week intervals")
     significantBreakpoints = []
     for i in range (1, len(timeline)-1):
         timeDiff = timeline[i+1].date - timeline[i].date
@@ -88,9 +115,9 @@ def locatePairedTwoWeekPlusIntervals(timeline):
             if (timeDiffPrev >= timedelta(days=14)) and timeline[i-1].active_devs >= 5:
 
                 # And we can now also confirm that we had a two week stable development period beforehand with which to compare, and can make note of it.
-                print("Significant period of development found surrounding " + str(timeline[i]))
-                print("Stable development from " + (str(timeline[i-1].date)) + " to " + (str(timeline[i].date)))
-                print("and from " + (str(timeline[i].date)) + " to " + (str(timeline[i+1].date)) + "\n")
+                # print("Significant period of development found surrounding " + str(timeline[i]))
+                # print("Stable development from " + (str(timeline[i-1].date)) + " to " + (str(timeline[i].date)))
+                # print("and from " + (str(timeline[i].date)) + " to " + (str(timeline[i+1].date)) + "\n")
                 significantBreakpoints.append((timeline[i-1], timeline[i], timeline[i+1]))
     return significantBreakpoints
 
@@ -100,13 +127,14 @@ def populateTimeline(author_objects):
         if (x.first_commit != x.last_commit):
             firstBreakpoint = TimelineBreakPoint(x.first_commit, 1, 0, x.name)
             secondBreakpoint = TimelineBreakPoint(x.last_commit, 0, 1, x.name)
-            print("Two breakpoints added. " + str(x.first_commit), str(x.last_commit))
+            # print("Two breakpoints added. " + str(x.first_commit), str(x.last_commit))
 
             # Sorting?
             timeline.append(firstBreakpoint)
             timeline.append(secondBreakpoint)
         else:
-            print("First and final commit are the same. No breakpoint to create.")
+            continue
+            # print("First and final commit are the same. No breakpoint to create.")
 
     new_list = sorted(timeline, key=lambda y: y.date, reverse=False)
 
@@ -118,7 +146,7 @@ def populateTimeline(author_objects):
         if z.was_final_commit:
             activeDevelopers -= 1
             z.active_devs = activeDevelopers
-        print(str(z))
+        # print(str(z))
 
     return new_list
 
