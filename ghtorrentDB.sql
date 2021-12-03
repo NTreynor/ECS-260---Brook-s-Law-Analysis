e0eec9043e93d99d56f6069b3e98783dc2baaf01
Lawrence Hwang <50124505+lawrence-hwang@users.noreply.github.com>
2021-12-03 05:17:41 -0800

    MySQL Query File
    Setup and query statements for searching the GHTorrent MySQL datadump

---
+++ ghtorrentDB.sql
@@ -0,0 +1,54 @@
+-- CREATE DATABASE GHTorrentDump;
+USE GHTorrentDump;
+-- SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', '')); -- Enable GROUP BY used in later queries
+
+-- // SETUP QUERIES
+-- // THIS GIVES SIZE OF YOUR CURRENT DATABASE
+-- SELECT table_schema "GHTorrentDump", sum( data_length + index_length ) / 1024 / 1024 "Data Base Size in MiB" 
+-- FROM information_schema.TABLES GROUP BY table_schema;  
+
+-- // THIS GIVES NUMBER OF ROWS IN WHATS SPECIFIED
+-- SELECT SUM(TABLE_ROWS)
+--    FROM INFORMATION_SCHEMA.TABLES
+--    WHERE TABLE_SCHEMA = 'GHTorrentDump'
+
+
+-- // ACTUAL QUERIES / PROGRESS //
+
+-- // QUERIES # OF USERS PER PROJECT ID //
+-- SELECT pj.repo_id, COUNT(pj.user_id) as UserCount
+-- FROM project_members pj 
+-- 	LEFT JOIN users u ON pj.user_id = u.id 
+-- GROUP BY pj.repo_id;
+
+-- // QUERIES URL, REPO ID (IN THIS DB), AND # OF USERS PER PROJECT ID //
+-- SELECT p.url, p.id, COUNT(pj.user_id) AS UserCount
+-- FROM project_members pj 
+-- 	LEFT JOIN users u ON pj.user_id = u.id 
+--     LEFT JOIN projects p ON pj.repo_id = p.id
+-- GROUP BY pj.repo_id;
+
+-- // QUERIES THE ABOVE QUERY BUT ONLY PROJECTS WITH > 5 USERS // 
+SELECT p.id, p.url, psub.UserCount
+FROM (
+	SELECT pj.repo_id, COUNT(pj.user_id) as UserCount
+	FROM project_members pj 
+		LEFT JOIN users u ON pj.user_id = u.id 
+	GROUP BY pj.repo_id
+    ) AS psub, projects p
+WHERE 
+	psub.repo_id = p.id AND
+    psub.UserCount >= 5
+;
+
+
+
+
+-- // GET INFO IF YOU HAVE A PROJECT ID - i pretty much only used these for sanity checking // 
+-- SELECT * -- Basic info about project p //
+-- FROM projects p
+-- WHERE p.id = 1;
+
+-- SELECT * -- Check which users belong to a given project ID, can use to ensure user count matches repo id // 
+-- FROM project_members pm, users u
+-- WHERE pm.repo_id = 1 AND pm.user_id = u.id;
